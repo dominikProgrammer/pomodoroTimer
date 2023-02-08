a50fb8cd6ede3307ab6ca84a845c686ae5a98f4b
domminiqq <dominik9051@gmail.com>
2023-02-08 14:08:47 +0100

    completion of work on the base version

--- static/script/main.js
+++ static/script/main.js
@@ -1,10 +1,11 @@
 class PomodoroTimer {
-    constructor(timers,sessions) {
+    constructor(timers) {
         this.timers = timers.map(time => {
             let [minutes, seconds] = time.split(':');
             return parseInt(minutes) * 60 + parseInt(seconds);
-            });
-        this.sessions = sessions;
+        });
+        this.sessions = this.timers.length;
+        console.log(this.sessions);
         this.time = document.getElementById('time');
         this.session = document.getElementById('session');
         this.next_time = document.getElementsByClassName('next_time')[0];
@@ -12,45 +13,64 @@ class PomodoroTimer {
         this.current_time = new Date().getTime();
         let seconds = parseInt(this.timers[0]);
         this.moment = this.current_time + seconds * 1000;
-        console.log(this.timers);
     }
 
     setTimer() {
         let diff = this.moment - this.current_time;
         let minutes = Math.floor(diff / 1000 / 60);
         let seconds = Math.floor((diff / 1000) % 60);
-        let formattedTime = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+        let formattedTime = (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
         this.time.innerText = formattedTime;
-        
+
         let nextTimeInSeconds = this.timers[1];
         let nextMinutes = Math.floor(nextTimeInSeconds / 60);
         let nextSeconds = nextTimeInSeconds % 60;
-        this.next_time.innerText = + (nextMinutes < 10 ? "0" : "") + nextMinutes + ":" + (nextSeconds < 10 ? "0" : "") + nextSeconds;        this.session.innerText = 'Session: 1/' + this.sessions;
+        if (isNaN(nextSeconds)) {
+            this.next_time.innerText = '00:00';
+        } else {
+            this.next_time.innerText = (nextMinutes < 10 ? "0" : "") + nextMinutes + ":" + (nextSeconds < 10 ? "0" : "") + nextSeconds;
+        }
+        this.session.innerText = 'Session: 1/' + this.sessions;
     }
 
     run() {
-    setInterval(() => {
-        this.current_time = new Date().getTime();
-        let diff = this.moment - this.current_time;
-        if (diff <= 0) {
-            this.current_session++;
-            if (this.current_session >= this.sessions) {
-                this.time.innerText = "Time's up";
-                this.time.style.fontSize = '100px';
-                return;
+        const timer_work = setInterval(() => {
+            this.current_time = new Date().getTime();
+            let diff = this.moment - this.current_time;
+            if (diff <= 0) {
+                this.current_session++;
+                if (this.current_session > this.sessions) {
+                    this.time.innerText = "Time's up";
+                    clearInterval(timer_work);
+                    return;
+                } else {
+                    let seconds = parseInt(this.timers[this.current_session - 1]);
+                    this.moment = this.current_time + seconds * 1000;
+
+                    let nextTimeInSeconds = this.timers[this.current_session];
+                    let nextMinutes = Math.floor(nextTimeInSeconds / 60);
+                    let nextSeconds = nextTimeInSeconds % 60;
+                    if (isNaN(nextSeconds)) {
+                        this.next_time.innerText = '00:00';
+                    } else {
+                        this.next_time.innerText = (nextMinutes < 10 ? "0" : "") + nextMinutes + ":" + (nextSeconds < 10 ? "0" : "") + nextSeconds;
+                    }
+                    this.session.innerText = 'Session: ' + this.current_session + '/' + this.sessions;
+                }
+            } else {
+                let minutes = Math.floor(diff / 1000 / 60);
+                let seconds = Math.floor((diff / 1000) % 60);
+                let formattedTime = (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+                this.time.innerText = formattedTime;
             }
-        }
-        
-        let minutes = Math.floor(diff / 1000 / 60);
-        let seconds = Math.floor((diff / 1000) % 60);
-        let formattedTime = (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-        this.time.innerText = formattedTime;
-    }, 1000);
-}
+        }, 100);
+    }
 }
 
-window.addEventListener('load', function() {
-    let timer = new PomodoroTimer(['00:05','5:00','5:00','10:00','40:00'],1);
+window.addEventListener('load', function () {
+
+    const link = this.window.location.href.split('#pomodoro')[1].split(';');
+    let timer = new PomodoroTimer(link);
     timer.setTimer();
     timer.run();
 
